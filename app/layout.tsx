11fc332aa6b0b1e687e354d7ce1293e922bd64c5
Sendjaa <dewa4388@gmail.com>
2026-03-30 19:35:40 +0700

    menambahkan metadata ke layout dan memperbarui versi dependensi @vercel/analytics di package.json dan package-lock.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type React from "react"
+import type { Metadata } from "next" // Tambahkan import ini
 import { Geist, Geist_Mono } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next"
@@ -6,6 +7,14 @@ import "./globalsMultimedia.css"
 
 const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "SoftEngineer™ | Portfolio & Digital Solutions",
+  description: "Jasa pembuatan website profesional dan solusi digital modern.",
+  verification: {
+    google: "4JUZL4x2RsH18J8sQiL2p9aCk9rrUEuIkEsQnTyzDc8",
+  },
+}
 
 export default function RootLayout({
   children,
